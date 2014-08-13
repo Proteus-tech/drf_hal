446ba53c0187645e41af1cbb9cec3c95a74d3693
pam <sinapam@proteus-agility.com>
2014-08-13 11:41:01 +0700

    changed version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 
 setup(
     name='drf-hal',
-    version='0.1.5',
+    version='0.1.6',
     description='Library that provides Hal specification capability on top of django-rest-framework',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
