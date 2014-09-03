3eb9ea0e85b6d2783dd04b4e5235373ebb5ebf92
pam <sinapam@proteus-agility.com>
2014-09-03 18:32:03 +0700

    limit version for djangorestframework to be less than 2.4.X because it's not compatible

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ from distutils.core import setup
 
 setup(
     name='drf-hal',
-    version='0.1.16',
+    version='0.1.17',
     description='Library that provides Hal specification capability on top of django-rest-framework',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
     url='http://proteus-tech.com',
     long_description=read('README.md'),
-    install_requires=['djangorestframework>=2.3.12'],
+    install_requires=['djangorestframework==2.3.14'],
     packages=['drf_hal'],
 )
 
