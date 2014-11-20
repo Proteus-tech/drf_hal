ddb34a33bb3bd441492c77ed20b92e13a3d4f2c8
pam <sinapam@proteus-agility.com>
2014-11-20 21:14:01 +0700

    change version to 0.2.1 (first small release)

--- setup.py
+++ setup.py
@@ -8,13 +8,12 @@ from distutils.core import setup
 
 setup(
     name='drf-hal',
-    version='0.1.25',
+    version='0.2.1',
     description='Library that provides Hal specification capability on top of django-rest-framework',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
     url='http://proteus-tech.com',
     long_description=read('README.md'),
-    install_requires=['djangorestframework==2.3.14'],
     packages=['drf_hal'],
 )
 
